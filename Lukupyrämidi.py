367010807926f236954f3bb9a583460575056f1e
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-05-16 20:29:41 +0300

    Update Lukupyrämidi.py

--- Lukupyrämidi.py
+++ Lukupyrämidi.py
@@ -1,10 +1,6 @@
-kerrokset = int(input("Syötä pyramidin kerrosten lukumäärä: "))
+kerrosten_lukumaara = int(input("Syötä pyramidin kerrosten lukumäärä: "))
 
-for i in range(1, kerrokset + 1):
-    for j in range(kerrokset - i):
-        print(" ", end="")
-    for k in range(2 * i - 1):
-        print("*", end="")
+for i in range(1, kerrosten_lukumaara + 1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
     print()
-
-input("Paina Enter lopettaaksesi...")
