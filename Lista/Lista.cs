8edf85dbf7d6378168e388eee8385b1db4705412
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-05-21 18:18:41 +0300

    lista

---
+++ Lista/Lista.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class lista
+{
+    static void Main()
+    {
+        List<int> luvut = new List<int>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Syötä kokonaisluku: ");
+            string syote = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(syote) && int.TryParse(syote, out int luku))
+            {
+                luvut.Add(luku);
+            }
+            else
+            {
+                Console.WriteLine("Virheellinen syöte. Syötä kokonaisluku.");
+                i--; // Vähennetään i:tä yhdellä, jotta käyttäjä voi yrittää uudestaan
+            }
+        }
+
+        Console.WriteLine("Luvut järjestyksessä:");
+
+        for (int i = 0; i < luvut.Count; i++)
+        {
+            Console.WriteLine(luvut[i]);
+        }
+    }
+}
