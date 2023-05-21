3fddc6d14ddc738a4323685a5c4e456d360a1a10
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-05-21 21:28:32 +0300

    ohjelma 2

---
+++ aliohjelma 2/ohjelma2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Ohjelma2
+{
+    static void Main()
+    {
+        Console.WriteLine("Anna 3 lukua, niin lasken niiden summan.");
+
+        int[] luvut = KysyLuvut();
+
+        int summa = LaskeSumma(luvut);
+
+        Console.WriteLine("Kolmen luvun " + TulostaLuvut(luvut) + " summa on: " + summa);
+    }
+
+    static int[] KysyLuvut()
+    {
+        int[] luvut = new int[3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write("Syötä luku " + (i + 1) + ": ");
+            luvut[i] = int.Parse(Console.ReadLine());
+        }
+
+        return luvut;
+    }
+
+    static int LaskeSumma(int[] luvut)
+    {
+        int summa = 0;
+
+        for (int i = 0; i < luvut.Length; i++)
+        {
+            summa += luvut[i];
+        }
+
+        return summa;
+    }
+
+    static string TulostaLuvut(int[] luvut)
+    {
+        string luvutString = "";
+
+        for (int i = 0; i < luvut.Length; i++)
+        {
+            luvutString += luvut[i];
+
+            if (i < luvut.Length - 2)
+            {
+                luvutString += ", ";
+            }
+            else if (i == luvut.Length - 2)
+            {
+                luvutString += " ja ";
+            }
+        }
+
+        return luvutString;
+    }
+}
