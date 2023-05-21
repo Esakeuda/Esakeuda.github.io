8d183cf2a728d1d9767dc8787f0514cc7de1f503
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-05-21 20:47:52 +0300

    aliohjelma

---
+++ aliohjelma1/Ohjelma.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Ohjelma
+{
+    static void Main()
+    {
+        Console.Write("Syötä kokonaisluku: ");
+        int luku = int.Parse(Console.ReadLine());
+        
+        int tulos = LaskeNelio(luku);
+        
+        Console.WriteLine("Luvun neliö on: " + tulos);
+    }
+    
+    static int LaskeNelio(int luku)
+    {
+        return luku * luku;
+    }
+}
