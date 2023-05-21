e1a05ef0107f3c99b85c166face3a6837f8c18da
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-05-21 17:53:58 +0300

    lukujensumma

---
+++ Lukujen summa/Lukujensumma.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class lukujensumma
+{
+    static void Main()
+    {
+        int summa = 0;
+        
+        while (summa < 100)
+        {
+            Console.Write("Syötä kokonaisluku: ");
+            int luku = int.Parse(Console.ReadLine());
+            
+            summa += luku;
+            Console.WriteLine("Summa: " + summa);
+        }
+        
+        Console.WriteLine("Raja on saavutettu!");
+    }
+}
+
