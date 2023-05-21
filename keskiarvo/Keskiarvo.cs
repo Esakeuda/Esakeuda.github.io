8cd50033796371c79cbcc304f59b67d9f42f42f5
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-05-21 17:41:04 +0300

    run

---
+++ keskiarvo/Keskiarvo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Keskiarvo
+{
+    static void Main()
+    {
+        int[] luvut = new int[5];
+        
+        for (int i = 0; i < luvut.Length; i++)
+        {
+            Console.Write("Syötä luku: ");
+            luvut[i] = int.Parse(Console.ReadLine());
+        }
+        
+        int summa = 0;
+        foreach (int luku in luvut)
+        {
+            summa += luku;
+        }
+        
+        double keskiarvo = (double)summa / luvut.Length;
+        
+        Console.WriteLine("Lukujen keskiarvo: " + keskiarvo);
+    }
+}
